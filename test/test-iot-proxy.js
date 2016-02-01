d7c7d2e3c4e7d1db3a66eed724a023ac7016f6e7
Serge Hänni <phylor@github.com>
2016-02-01 15:16:01 +0100

    Added routing of additional text in GET URLs

--- test/test-iot-proxy.js
+++ test/test-iot-proxy.js
@@ -1,13 +1,19 @@
-var expect = require("chai").expect;
-var request = require("supertest");
+var chai = require('chai');
+var expect = chai.expect;
 var iotproxy = require("../iotproxy");
 var async = require('async');
+var testServer = require('../lib/test-server');
+var should = chai.should();
+var chaiHttp = require('chai-http');
+chai.config.includeStack = true;
+chai.use(chaiHttp);
 
-var app = request('http://localhost:8000');
+var app = chai.request('http://localhost:8000');
 
 describe("iot-proxy", function() {
   before(function (done) {
     iotproxy.start(function() {
+      testServer.startTestServerOnPort(4000);
       done();
     });
   });
@@ -16,9 +22,10 @@ describe("iot-proxy", function() {
     it("should start an HTTP server on default port", function(done) {
       app
         .get('/')
-        .expect('Content-Type', /text/)
-        .expect('404')
-        .expect(404, done)
+        .end(function(err, res) {
+          res.should.have.status(404);
+          done();
+        });
     });
   });
 
@@ -26,7 +33,29 @@ describe("iot-proxy", function() {
     it("should return a response for a valid service", function(done) {
       app
         .get('/weather')
-        .expect(200, done);
+        .end(function(err, res) {
+          res.should.have.status(200);
+          done();
+        });
+    });
+
+    it('should be able to talk to the test server', function(done) {
+      app.get('/testserver')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        expect(res.body.message).to.equal('Test API');
+        done();
+      });
+    });
+
+    it('should return a response for a concatinated GET request', function(done) {
+      app
+        .get('/weather/zurich/forecast')
+        .end(function(err, res) {
+          res.should.have.status(200);
+          expect(res.body.message).to.equal('Forecast for Zurich');
+          done();
+        });
     });
   });
 });
